75e359b21878d8a3244c4ed5edf31c608ed5e9a3
Mede <dalemale09@gmail.com>
2024-08-16 10:57:37 +0200

    user id null

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'dart:async';
 import 'user_list_screen.dart';
 import 'topic_screen.dart';
 import 'category_screen.dart';
@@ -98,66 +98,72 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // Dialog de connexion
-  void _showLoginDialog(BuildContext context) {
-    final TextEditingController _usernameController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
+Future<bool> _showLoginDialog(BuildContext context) {
+  final Completer<bool> completer = Completer<bool>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Connexion'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Nom d\'utilisateur',
-                ),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Connexion'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Nom d\'utilisateur',
               ),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Mot de passe',
-                ),
-                obscureText: true,
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Annuler'),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                String? token = await AuthService().login(
-                  _usernameController.text,
-                  _passwordController.text,
-                );
-                if (token != null) {
-                  setState(() {
-                    isLoggedIn = true;
-                  });
-                  Navigator.of(context).pop();
-                } else {
-                  // Affiche un message d'erreur
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text('Échec de la connexion. Veuillez réessayer.'),
-                  ));
-                }
-              },
-              child: Text('Connexion'),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+              ),
+              obscureText: true,
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              completer.complete(false); // Login failed or cancelled
+            },
+            child: Text('Annuler'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              String? token = await AuthService().login(
+                _usernameController.text,
+                _passwordController.text,
+              );
+              if (token != null) {
+                setState(() {
+                  isLoggedIn = true;
+                });
+                Navigator.of(context).pop();
+                completer.complete(true); // Login successful
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Échec de la connexion. Veuillez réessayer.'),
+                ));
+                completer.complete(false); // Login failed
+              }
+            },
+            child: Text('Connexion'),
+          ),
+        ],
+      );
+    },
+  );
+
+  return completer.future;
+}
+
+
 
   // Dialog d'inscription
   void _showRegisterDialog(BuildContext context) {
@@ -234,82 +240,96 @@ class _HomeScreenState extends State<HomeScreen> {
     return response.statusCode == 201;
   }
 
-  void _showCreateTopicDialog(BuildContext context) {
-    final TextEditingController _topicTitleController = TextEditingController();
-    final TextEditingController _topicContentController = TextEditingController();
-    int _selectedCategoryId = 1;
+void _showCreateTopicDialog(BuildContext context) async {
+  if (!isLoggedIn) {
+    // Si l'utilisateur n'est pas connecté, afficher le dialogue de connexion
+    bool success = await _showLoginDialog(context);
+    if (!success) {
+      return; // Si l'utilisateur annule ou échoue la connexion, retourner.
+    }
+  }
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Créer une nouvelle discussion'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _topicTitleController,
-                decoration: InputDecoration(
-                  labelText: 'Titre de la discussion',
-                ),
+  _showTopicCreationDialog(context);
+}
+
+void _showTopicCreationDialog(BuildContext context) {
+  final TextEditingController _topicTitleController = TextEditingController();
+  final TextEditingController _topicContentController = TextEditingController();
+  int _selectedCategoryId = 1;
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Créer une nouvelle discussion'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _topicTitleController,
+              decoration: InputDecoration(
+                labelText: 'Titre de la discussion',
               ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _topicContentController,
-                decoration: InputDecoration(
-                  labelText: 'Contenu de la discussion',
-                ),
-                maxLines: 5,
-              ),
-              SizedBox(height: 16.0),
-              DropdownButtonFormField<int>(
-                value: _selectedCategoryId,
-                onChanged: (int? newValue) {
-                  setState(() {
-                    _selectedCategoryId = newValue!;
-                  });
-                },
-                items: [
-                  DropdownMenuItem(value: 1, child: Text('Discussions')),
-                  DropdownMenuItem(value: 2, child: Text('Bible')),
-                  DropdownMenuItem(value: 3, child: Text('Vos Réseaux')),
-                ],
-                decoration: InputDecoration(
-                  labelText: 'Choisir une catégorie',
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Annuler'),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                String topicTitle = _topicTitleController.text;
-                String topicContent = _topicContentController.text;
-                bool success = await _createTopic(topicTitle, topicContent, _selectedCategoryId);
-                if (success) {
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text('Discussion créée avec succès'),
-                  ));
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text('Erreur lors de la création de la discussion'),
-                  ));
-                }
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _topicContentController,
+              decoration: InputDecoration(
+                labelText: 'Contenu de la discussion',
+              ),
+              maxLines: 5,
+            ),
+            SizedBox(height: 16.0),
+            DropdownButtonFormField<int>(
+              value: _selectedCategoryId,
+              onChanged: (int? newValue) {
+                setState(() {
+                  _selectedCategoryId = newValue!;
+                });
               },
-              child: Text('Créer'),
+              items: [
+                DropdownMenuItem(value: 1, child: Text('Discussions')),
+                DropdownMenuItem(value: 2, child: Text('Bible')),
+                DropdownMenuItem(value: 3, child: Text('Vos Réseaux')),
+              ],
+              decoration: InputDecoration(
+                labelText: 'Choisir une catégorie',
+              ),
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Annuler'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              String topicTitle = _topicTitleController.text;
+              String topicContent = _topicContentController.text;
+              bool success = await _createTopic(topicTitle, topicContent, _selectedCategoryId);
+              if (success) {
+                Navigator.of(context).pop();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Discussion créée avec succès'),
+                ));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Erreur lors de la création de la discussion'),
+                ));
+              }
+            },
+            child: Text('Créer'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
 
   Future<bool> _createTopic(String title, String description, int categoryId) async {
     final response = await http.post(
